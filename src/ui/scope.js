7648f128462c01358f4186477bbbf8f460beda91
Lu_lucica <275277398+joy1723ce-byte@users.noreply.github.com>
2026-08-28 10:26:09 +0900

    卵状態の追加

--- src/ui/scope.js
+++ src/ui/scope.js
@@ -11,21 +11,35 @@ const CX = 200, CY = 168;   // スコープの中心
 
 /** tornado画像読み込み */
 export function scopeSVG() {
+  const days = S.streak.currentDays;
+
+  // 0日目は卵
+  if (days === 0) {
+    return `
+      <div class="tornado-images">
+        <div class="face-images egg-state">
+          <img class="egg-image" src="./assets/face/egg.png" alt="">
+        </div>
+      </div>
+    `;
+  }
+
+  // 1日目以降は災害を表示
   const lv = Math.max(1, S.disaster.level);
   const band = bandOf(lv);
   const bandNo = BANDS.indexOf(band) + 1;
 
   return `
     <div class="tornado-images">
-      <img class="tornado-image" src="./assets/tornado/${bandNo}-1.png" alt="" >
-      <img class="tornado-image" src="./assets/tornado/${bandNo}-2.png" alt="" >
+      <img class="tornado-image" src="./assets/tornado/${bandNo}-1.png" alt="">
+      <img class="tornado-image" src="./assets/tornado/${bandNo}-2.png" alt="">
 
       <div class="face-images">
-      <img class="face-image face-m1" src="./assets/face/${bandNo}-m1.png" alt="" >
-      <img class="face-image face-m2" src="./assets/face/${bandNo}-m2.png" alt="" >
-      <img class="face-image face-m3" src="./assets/face/${bandNo}-m3.png" alt="" >
+        <img class="face-image face-m1" src="./assets/face/${bandNo}-m1.png" alt="">
+        <img class="face-image face-m2" src="./assets/face/${bandNo}-m2.png" alt="">
+        <img class="face-image face-m3" src="./assets/face/${bandNo}-m3.png" alt="">
       </div>
-    </div> 
+    </div>
   `;
 }
 
